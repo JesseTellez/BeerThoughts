ea122ab79dd30d152ddda2a4bf87c73359f95657
Jesse Tellez <jessetellez@Jesses-MacBook-Pro-2.local>
2016-06-10 21:52:49 -0600

    added dots asset

---
+++ Beer Thoughts/Beer Thoughts/BeerCell.swift
@@ -0,0 +1,12 @@
+//
+//  BeerCell.swift
+//  Beer Thoughts
+//
+//  Created by Jesse Tellez on 6/10/16.
+//  Copyright © 2016 HomeBrew. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
