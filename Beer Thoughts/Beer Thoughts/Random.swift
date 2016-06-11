47bf87b61cf6c1df0daf285f8ed70889d8937383
yuffieh <st.huynh@ymail.com>
2016-06-10 23:57:24 -0600

    added random file

---
+++ Beer Thoughts/Beer Thoughts/Random.swift
@@ -0,0 +1,13 @@
+//
+//  Random.swift
+//  Beer Thoughts
+//
+//  Created by Serah Huynh on 6/10/16.
+//  Copyright © 2016 HomeBrew. All rights reserved.
+//
+
+import Foundation
+
+class Random {
+    
+}
