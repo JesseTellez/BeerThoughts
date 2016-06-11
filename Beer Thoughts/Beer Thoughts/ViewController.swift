cf26178ca3ac9146a76a035d16d8581c8f1860c9
Jesse Tellez <jessetellez@Jesses-MacBook-Pro-2.local>
2016-06-10 21:18:42 -0600

    Created Starter project for BeerThoughts

---
+++ Beer Thoughts/Beer Thoughts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beer Thoughts
+//
+//  Created by Jesse Tellez on 6/10/16.
+//  Copyright © 2016 HomeBrew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
