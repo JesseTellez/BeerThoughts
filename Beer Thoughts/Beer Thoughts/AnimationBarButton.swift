e6f9d30bfdcced1ea188d346f5e3d4cbb78172b9
Jesse Tellez <jessetellez@Jesses-MacBook-Pro-2.local>
2016-06-21 09:27:57 -0600

    Added bar button functionality and data passing

---
+++ Beer Thoughts/Beer Thoughts/AnimationBarButton.swift
@@ -0,0 +1,9 @@
+//
+//  AnimationBarButton.swift
+//  Beer Thoughts
+//
+//  Created by Jesse Tellez on 6/20/16.
+//  Copyright © 2016 HomeBrew. All rights reserved.
+//
+
+import Foundation
---
+++ Beer Thoughts/Beer Thoughts/Rotatable.swift
@@ -0,0 +1,9 @@
+//
+//  Rotatable.swift
+//  Beer Thoughts
+//
+//  Created by Jesse Tellez on 6/20/16.
+//  Copyright © 2016 HomeBrew. All rights reserved.
+//
+
+import Foundation
